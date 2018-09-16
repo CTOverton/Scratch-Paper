6fa718692d5adab2f240f9d6fc6ff7db89f8e4ba
Christian Overton <c@ctoverton.com>
2018-09-15 23:49:14 -0400

    Working on Saving Layout

--- popup.js
+++ popup.js
@@ -34,16 +34,54 @@ clipboard.on('error', function(e) {
 let addbtn = document.getElementById('add_btn');
 let tools = document.getElementById('sp_tools');
 
-addbtn.onclick = function(element) {
-    tools.innerHTML +=
-    '<div class="sp_text">\n' +
-        '            <input type="text">\n' +
-        '            <button class="copy_btn" data-clipboard-action="copy">\n' +
-        '                <i class="material-icons md-18">file_copy</i>\n' +
-        '            </button>\n' +
-        '            <button class="btn less">\n' +
-        '                <i class="material-icons md-18">more_horiz</i>\n' +
-        '            </button>\n' +
-        '        </div>'
-    ;
+addbtn.onclick = function() {
+    $("#sp_tools").append($("#template").html());
+};
+
+addListeners();
+
+function addListeners() {
+    let removebtn = $('.remove_btn');
+
+    if (removebtn) {
+        for (var i=0; i < removebtn.length; i++)
+        removebtn[i].onclick = function(element) {
+            this.parentElement.remove();
+        };
+    }
+};
+
+let save = document.getElementById('save');
+let load = document.getElementById('load');
+
+save.onclick = function() {
+
+    var $content = $("#sp_tools");
+
+    var $clone = $content.clone()
+    $clone.find(':input', ':textarea').each(function () {
+        var $input = $(this);
+        // start an attribute object later use with attr()
+        var attrs = {
+            value: $input.val()
+        };
+
+        // add the attributes to element
+        $input.attr(attrs);
+    });
+
+    var html = $clone.html();
+
+    chrome.storage.sync.set({layout: html}, function() {
+        console.log('layout is:');
+        console.log('');
+        console.log(html);
+    })
+};
+
+load.onclick = function() {
+    console.log('loading...');
+    chrome.storage.sync.get('layout', function(data) {
+        tools.innerHTML = data.layout;
+    });
 };
